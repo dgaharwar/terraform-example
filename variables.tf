7b7661104ac820b1be76e421ef5c08db9642aa55
S <thavamani.raja@hpe.com>
2020-05-22 09:45:09 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,6 @@
+# Copyright 2020 Hewlett Packard Enterprise Development LP
+variable "vsphere_server" {}
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "template_username" {}
+variable "template_password" {}
