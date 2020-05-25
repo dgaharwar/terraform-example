8fb8811e14d6ef17ec122df496b4eb259daa97e8
S <thavamani.raja@hpe.com>
2020-05-25 14:42:00 +0530

    initial_commit

--- modules/api_tier/outputs.tf
+++ modules/api_tier/outputs.tf
@@ -3,7 +3,7 @@ output "app_instance_id" {
   value = "${vsphere_virtual_machine.app.id}"
 }
 
-output "app_instance_private_ip" {
-  value = "${vsphere_virtual_machine.app.default_ip_address}"
-}
+#output "app_instance_private_ip" {
+#  value = "${vsphere_virtual_machine.app.default_ip_address}"
+#}
 
--- modules/database_tier/outputs.tf
+++ modules/database_tier/outputs.tf
@@ -3,7 +3,7 @@ output "db_instance_id" {
   value = "${vsphere_virtual_machine.db.id}"
 }
 
-output "db_instance_private_ip" {
-  value = "${vsphere_virtual_machine.db.default_ip_address}"
-}
+#output "db_instance_private_ip" {
+#  value = "${vsphere_virtual_machine.db.default_ip_address}"
+#}
 
